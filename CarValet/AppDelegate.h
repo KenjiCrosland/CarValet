9fec2082e13dc1beaa7b06d3b09af87d9dc98fd8
KenjiCrosland <kenji.crosland@gmail.com>
2014-09-24 20:01:07 -0700

    Initial Commit

---
+++ CarValet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarValet
+//
+//  Created by Kenji Crosland on 9/24/14.
+//  Copyright (c) 2014 Kenji Crosland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CarValet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CarValet
+//
+//  Created by Kenji Crosland on 9/24/14.
+//  Copyright (c) 2014 Kenji Crosland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
