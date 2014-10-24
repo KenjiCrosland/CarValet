2d2b7c0cfa49361117311cd05fce9c60e2f24df3
Kenji Crosland Commit <Matt@Matts-MacBook-Air.local>
2014-10-23 17:21:27 -0700

    Added Car Edit View Controller

---
+++ CarValet/CarEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  CarEditViewController.h
+//  CarValet
+//
+//  Created by Kenji Crosland on 10/23/14.
+//  Copyright (c) 2014 Kenji Crosland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarEditViewController : UIViewController
+
+@end
---
+++ CarValet/CarEditViewController.m
@@ -0,0 +1,37 @@
+//
+//  CarEditViewController.m
+//  CarValet
+//
+//  Created by Kenji Crosland on 10/23/14.
+//  Copyright (c) 2014 Kenji Crosland. All rights reserved.
+//
+
+#import "CarEditViewController.h"
+
+@interface CarEditViewController ()
+
+@end
+
+@implementation CarEditViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
