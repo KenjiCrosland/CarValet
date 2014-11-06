2289b8a14d09934e26722049c9be9b96aa7728f3
Kenji Crosland Commit <Matt@Matts-MacBook-Air.local>
2014-11-05 18:24:29 -0800

    Added Car Edit Properties
    For the Edit View. Currency is still nil.

--- CarValet/CarEditViewController.h
+++ CarValet/CarEditViewController.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+@class Car;
+
 @interface CarEditViewController : UIViewController
+@property (nonatomic) NSInteger carNumber;
+@property (strong, nonatomic) Car *currentCar;
+
+@property (weak, nonatomic) IBOutlet UILabel *carNumberLabel;
+@property (weak, nonatomic) IBOutlet UITextField *modelField;
+@property (weak, nonatomic) IBOutlet UITextField *makeField;
+@property (weak, nonatomic) IBOutlet UITextField *yearField;
+@property (weak, nonatomic) IBOutlet UITextField *fuelField;
 
 @end
--- CarValet/CarEditViewController.m
+++ CarValet/CarEditViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CarEditViewController.h"
+#import "Car.h"
 
 @interface CarEditViewController ()
 
@@ -16,7 +17,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSString *carNumberText;
+    carNumberText = [NSString stringWithFormat:@"Car Number: %ld", (long)self.carNumber];
+    self.carNumberLabel.text = carNumberText;
+    self.modelField.text = self.currentCar.model;
+    self.makeField.text = self.currentCar.make;
+    self.yearField.text = [NSString stringWithFormat:@"%d", self.currentCar.year];
+    self.fuelField.text = [NSString stringWithFormat:@"%0.2f", self.currentCar.fuelAmount];
+    
     // Do any additional setup after loading the view.
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    
+    self.currentCar.make = self.makeField.text;
+    self.currentCar.model = self.modelField.text;
+    self.currentCar.year = [self.yearField.text intValue];
+    self.currentCar.fuelAmount = [self.fuelField.text floatValue];
 }
 
 - (void)didReceiveMemoryWarning {
